f296f34f4249db2442ad50f18cd2c8cb14edeb25
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-06-03 15:25:59 -0300

    Create Instrucoes.h

---
+++ Instrucoes.h
@@ -0,0 +1,19 @@
+#ifndef INSTRUçõES_H
+#define INSTRUçõES_H
+#include<SFML/Graphics.hpp>
+
+
+class Instrucoes
+{
+    public:
+        Instrucoes(float width, float height);
+        ~Instrucoes();
+
+        void printtela(sf::RenderWindow* window, int* b);
+
+    private:
+        sf::Font font1;
+        sf::Text text1;
+};
+
+#endif // INSTRUçõES_H
