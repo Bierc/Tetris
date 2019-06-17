649eb10e3cf07b3fbbad1c3618d68bd4b13326b8
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-06-17 15:19:51 -0300

    Create Pecas.h

---
+++ Pecas.h
@@ -0,0 +1,39 @@
+#ifndef PECAS_H
+#define PECAS_H
+#include<SFML/Graphics.hpp>
+
+
+
+class Pecas
+{
+
+    public:
+		Pecas();
+		void rotate();
+		void Moving(int k);
+		void draw(sf::RenderWindow *window );
+		void verificaMovimento(sf::Event event);
+		struct posicao
+		{
+			int x;
+			int y;
+		}a[4] , b[4];
+
+    private:
+		sf::Texture text;
+		sf::Sprite s;
+		
+		int Figuras[7][4]{
+			1,3,5,7,
+			2,4,5,7,
+			3,5,4,6,
+			3,5,4,7,
+			2,3,5,7,
+			3,5,7,6,
+			2,3,4,5,
+			};
+
+
+};
+
+#endif // PECAS_H
