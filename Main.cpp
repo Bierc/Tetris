7167e4aa3baaeb7a3229bf8defdaa88d1345372f
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-06-18 21:25:31 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -10,75 +10,34 @@ using namespace std;
 
 int main()
 {
-
     sf::RenderWindow window(sf::VideoMode(800,800), "Tetris");
-    Menu menu(window.getSize().x, window.getSize().y);
     Instrucoes inst(window.getSize().x, window.getSize().y);
-    Jogo jogo(window.getSize().x, window.getSize().y);
-    sf::Music music;
-	Pecas p;
-    int a=0;
-	if (!music.openFromFile("music_menu.ogg"))
-		return -1; // error
-    music.play();
+    Menu menu(window.getSize().x, window.getSize().y);
+	Jogo jogo(window.getSize().x, window.getSize().y);
+    int a = 0 ;
     while(window.isOpen()) {
     while(1) {
 
     switch(a) {
     case 0:
+	{
+		a = menu.Run(window);
+		break;
+	}
+        
 
-        sf::Event event;
+	case 1: {
+		
+		inst.printtela(&window, &a);
+	}
+		break;
 
-        while(window.pollEvent(event)) {
 
-            switch(event.type) {
-            case sf::Event::KeyReleased:
-                switch(event.key.code) {
-                case sf::Keyboard::Up:
-                    menu.MoveUp();
-                    break;
-                case sf::Keyboard::Down:
-                    menu.MoveDown();
-                    break;
-                case sf::Keyboard::Return:
-                    switch(menu.GetPressedItem()) {
-                    case 0:
-                        a=4;
-                        break;
-                    case 1:
-                        a=1;
-                        break;
-                    case 2:
-                        window.close();
-                        break;
-                    case 3:
-                        a=2;
-                        break;
-                    }
-                }
-
-                break;
-
-            case sf::Event::Closed:
-                window.close();
-                break;
-            }
-        }
-        window.clear(sf::Color::Blue);
-        menu.draw(window);
-        window.display();
-
-        break;
-
-    case 1:
-        inst.printtela(&window, &a);
-        break;
-
-    case 4:
-		music.stop();
-		jogo.printtela(&window, &a, event);
-		music.play();
-        break;
+	case 4: {
+		
+		jogo.RunGame(window, &a);
+	}
+		break;
 
 
     }
@@ -89,3 +48,4 @@ int main()
     }
          return 0;
         }
+
