572fc34f12b8738e53d2c3f5371e745cefdf4fba
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-06-18 21:21:36 -0300

    Update Jogo.cpp

--- Jogo.cpp
+++ Jogo.cpp
@@ -1,9 +1,10 @@
 #include "..\include\Jogo.h"
 #include<SFML/Graphics.hpp>
+#include<time.h>
 
 Jogo::Jogo(float width , float height)
 {
-	
+
 }
 
 Jogo::~Jogo()
@@ -13,14 +14,46 @@ Jogo::~Jogo()
 
 
 
-    void Jogo::printtela(sf::RenderWindow* window, int* b, sf::Event event) {
-        do{
-        window ->clear(sf::Color::White);
-		pe.verificaMovimento(event);
-		pe.draw(window);
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            *b = 0;
-        }
-        }while(*b != 0);
+void Jogo::RunGame(sf::RenderWindow &window , int *b) {
+	sf::Event game_event;
+	int x = 0;
+	int n = 3;
+	bool rotacionar = 0;
+	pe.Bulding(n);
+	//pe.draw(window);
+	do{
+		
+		
+		
+		while (window.pollEvent(game_event))
+		{
+			if (game_event.type == sf::Event::KeyPressed) {
 
-    }
+				switch (game_event.key.code)
+				{
+				case sf::Keyboard::Left:
+					x = -1;
+					pe.Moving(x);
+					break;
+				case sf::Keyboard::Right:
+					x = 1;
+					pe.Moving(x);
+					break;
+				case sf::Keyboard::Space:
+					pe.rotate();
+					break;
+				}
+			}
+		
+		}
+		
+		window.clear(sf::Color::White);
+		pe.draw(window);
+		window.display();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+			*b = 0;
+		}
+	} while (*b != 0);
+}
+
+
