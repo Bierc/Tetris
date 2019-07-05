6e30f2bec1428d9453abdf95326a0484c77602a9
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-07-05 18:57:23 -0300

    Update Jogo.cpp

--- Jogo.cpp
+++ Jogo.cpp
@@ -2,64 +2,64 @@
 #include<SFML/Graphics.hpp>
 #include<time.h>
 
-Jogo::Jogo(float width, float height)
+Jogo::Jogo(float width , float height)
 {
-
+	// ...
 }
 
 Jogo::~Jogo()
 {
-	//dtor
+    //dtor
 }
 
 
 
-void Jogo::RunGame(sf::RenderWindow &window, int *b) {
+void Jogo::RunGame(sf::RenderWindow &window , int *b) {
 	sf::Event game_event;
 	int x = 0;
-	
+	int n = rand() % 7;
 	bool rotacionar = 0;
 	float tempo = 0;
-	int n = rand() % 7;
-	float delay = 0.3;
-	sf::Clock clock;
+	float delay = 0.3f;
+	//srand(time(0));
 	pe.Bulding(n);
-	do {
+	
+	do{
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		tempo += time;
+
+		pe.down(tempo, delay);
 		
-		
-			float time = clock.getElapsedTime().asSeconds();
-			clock.restart();
-			tempo += time;
+		while (window.pollEvent(game_event))
+		{
+			if (game_event.type == sf::Event::KeyPressed) {
 
-			pe.down(tempo, delay);
-
-			while (window.pollEvent(game_event))
-			{
-				if (game_event.type == sf::Event::KeyPressed) {
-
-					switch (game_event.key.code)
-					{
-					case sf::Keyboard::Left:
-						x = -1;
-						pe.Moving(x);
-						break;
-					case sf::Keyboard::Right:
-						x = 1;
-						pe.Moving(x);
-						break;
-					case sf::Keyboard::Space:
-						pe.rotate();
-						break;
-					}
+				switch (game_event.key.code)
+				{
+				case sf::Keyboard::Left:
+					x = -1;
+					pe.Moving(x);
+					break;
+				case sf::Keyboard::Right:
+					x = 1;
+					pe.Moving(x);
+					break;
+				case sf::Keyboard::Space:
+					pe.rotate();
+					break;
 				}
-
 			}
-
-			window.clear(sf::Color::White);
-			pe.draw(window);
-			window.display();
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-				*b = 0;
-			}
-		} while (*b != 0 );
+		
+		}	
+		x = 0; rotacionar = 0; delay = 0.1f;
+		window.clear(sf::Color::White);
+		pe.Funcao(window);
+		pe.draw(window);
+		window.display();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+			*b = 0;
+		}
+	} while (*b != 0);
 }
+
