9df49890b65b5b85365003129c0b69dc51fd7119
Yuguardion <48190224+Yuguardion@users.noreply.github.com>
2019-07-05 16:05:31 -0300

    Update Pecas.h

--- Pecas.h
+++ Pecas.h
@@ -1,5 +1,5 @@
-#ifndef PECAS_H
-#define PECAS_H
+#pragma once
+
 #include<SFML/Graphics.hpp>
 
 
@@ -9,38 +9,37 @@
 class Pecas
 {
 
-    public:
-		Pecas();
-		void rotate();
-		void Moving(int k);
-		void draw(sf::RenderWindow &window );
-		void Bulding(int k);
-		void down(float &k , float b);
-		bool check();
-		int campo[20][10] = { 0 };
-		struct posicao
-		{
-			int x;
-			int y;
-		}a[4] , b[4];
+public:
+	Pecas();
+	int colorNum;
+	void rotate();
+	void Moving(int k);
+	void draw(sf::RenderWindow &window);
+	void Bulding(int k);
+	void down(float &k, float b);
+	bool check();
+	int campo[20][10] = { 0 };
+	struct posicao
+	{
+		int x;
+		int y;
+	}a[4], b[4];
 
-    private:
-		sf::Texture text;
-		sf::Sprite s;
-		const int Y = 20;
-		const int X = 10;
-		int Figuras[7][4]{
-			1,3,5,7,
-			2,4,5,7,
-			3,5,4,6,
-			3,5,4,7,
-			2,3,5,7,
-			3,5,7,6,
-			2,3,4,5,
-			};
+private:
+	sf::Texture text;
+	sf::Sprite s;
+	const int Y = 20;
+	const int X = 10;
+	int Figuras[7][4]{
+		1,3,5,7,
+		2,4,5,7,
+		3,5,4,6,
+		3,5,4,7,
+		2,3,5,7,
+		3,5,7,6,
+		2,3,4,5,
+	};
 
 
 };
-
-#endif // PECAS_H
 
