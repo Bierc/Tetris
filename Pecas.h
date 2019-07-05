8bd4e91b8b87616e4522da3887500cb29abe598f
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-07-05 18:57:55 -0300

    Update Pecas.h

--- Pecas.h
+++ Pecas.h
@@ -9,37 +9,40 @@
 class Pecas
 {
 
-public:
-	Pecas();
-	int colorNum;
-	void rotate();
-	void Moving(int k);
-	void draw(sf::RenderWindow &window);
-	void Bulding(int k);
-	void down(float &k, float b);
-	bool check();
-	int campo[20][10] = { 0 };
-	struct posicao
-	{
-		int x;
-		int y;
-	}a[4], b[4];
+    public:
+		Pecas();
+		void rotate();
+		void Moving(int k);
+		void draw(sf::RenderWindow &window );
+		void Bulding(int k);
+		void down(float &k , float c);
+		bool check();
+		void Funcao(sf::RenderWindow &window);
+		int campo[20][10] = { 0 };
+		struct posicao
+		{
+			int x;
+			int y;
+		}a[4] , b[4];
+		int ColorNum = 1;
 
-private:
-	sf::Texture text;
-	sf::Sprite s;
-	const int Y = 20;
-	const int X = 10;
-	int Figuras[7][4]{
-		1,3,5,7,
-		2,4,5,7,
-		3,5,4,6,
-		3,5,4,7,
-		2,3,5,7,
-		3,5,7,6,
-		2,3,4,5,
-	};
+    private:
+		sf::Texture text;
+		sf::Sprite s;
+		const int M = 20;
+		const int N = 10;
+		int Figuras[7][4]{
+			1,3,5,7,
+			2,4,5,7,
+			3,5,4,6,
+			3,5,4,7,
+			2,3,5,7,
+			3,5,7,6,
+			2,3,4,5,
+			};
 
 
 };
+
+#endif // PECAS_H
 
