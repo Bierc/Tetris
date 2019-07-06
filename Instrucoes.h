257149ded4f869c5fc5598de64f508f8e562ac82
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-07-06 14:02:27 -0300

    Update Instrucoes.h

--- Instrucoes.h
+++ Instrucoes.h
@@ -8,12 +8,14 @@ class Instrucoes
     public:
         Instrucoes(float width, float height);
         ~Instrucoes();
-
         void printtela(sf::RenderWindow* window, int* b);
 
     private:
         sf::Font font1;
-        sf::Text text1;
+		sf::Text text1;
+		sf::Text text2;
+		sf::Text text3;
+		sf::Text text4;
 };
 
 #endif // INSTRUçõES_H
