729e029cefb8e62932c33a58fd6bc3318ce8886d
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-07-06 09:02:26 -0300

    Update Jogo.h

--- Jogo.h
+++ Jogo.h
@@ -5,7 +5,8 @@
 #include"Pecas.h"
 #include"Instrucoes.h"
 #include"Recordes.h"
-
+#include<stdio.h>
+#include <iostream>
 
 class Jogo
 {
@@ -13,7 +14,7 @@ class Jogo
        // void Tabuleiro(int width, int height);
 		void RunGame(sf::RenderWindow &window , int *b);
 		Jogo(float width , float height);
-
+		int Pause(sf::RenderWindow &window);
         ~Jogo();
 
 
@@ -22,7 +23,9 @@ class Jogo
 		sf::Music music_game;
 		Recordes rec;
 		sf::Clock clock;
+		sf::Font font;
 		sf::Text score;
+		int pontos = 0;
 
 };
 
