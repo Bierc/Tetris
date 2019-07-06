b4eb7e3998ba3004a74c4912b5e9bc6dd81f3707
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2019-07-06 14:04:29 -0300

    Create recordes.h

---
+++ recordes.h
@@ -0,0 +1,38 @@
+#ifndef RECORDES_H
+#define RECORDES_H
+#include<string>
+#include<iostream>
+#include<SFML/Graphics.hpp>
+#include <stdio.h>
+#define MAX 5
+
+using namespace std;
+using std::string;
+
+
+class Recordes
+{
+    public:
+		void iniciar(float width, float height);
+        ~Recordes();
+        void verifica(sf::RenderWindow &window, int *a);
+        void EventName(sf::RenderWindow &window);
+		void LeRecordes(sf::RenderWindow &window, int *b);
+		struct jogador {
+			char name[5];
+			int pontuacao;
+		}p[5] , j[5];
+
+    private:
+        int score;
+		sf::Text name_record;
+		sf::Font font_recordes;
+		sf::Text text_recordes[5];
+		sf::Text info; //texto que vai informar a posiçao , nome e pontuação
+		char name[5];
+
+
+		
+};
+
+#endif // RECORDES_H
